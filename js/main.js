a6ba2f79358799e052a60a8a5105fdc91fab077e
Todu66 <giga.todua06@gmail.com>
2022-10-12 23:43:34 +0400

    start of html

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// const answer = document.getElementById('bill');
+// const reset = document.getElementById('reset');
+// const alertZero = document.querySelector('.alert-zero');
+// const percents = document.querySelectorAll('.same');
+
+// let
+
+let tipButton = document.querySelectorAll(".same");
+
+// answer.addEventListener('input', function () {
+//     if(answer.value >= 1) {
+//         document.querySelector('.top-right').textContent =
+//     }
+// })
+function zeroCheck() {
+  // answer. ('click', function () {
+  //     if(answer.value.length == '') {
+  //         alertZero.style.display = 'inline';
+  //         alertZero.style.color = '#e17457';
+  //     }
+  // })
+}
+
+}
+
+removeClick();
+
+zeroCheck();
