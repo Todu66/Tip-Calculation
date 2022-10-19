ca3fd9d3d8e76c3216e0db10625812f035b4e29f
Todu66 <giga.todua06@gmail.com>
2022-10-19 12:24:44 +0400

    making logic

--- js/main.js
+++ js/main.js
@@ -5,25 +5,42 @@ const persentCal = document.getElementsByClassName("same");
 // ----------------------------------------------------
 // ----------------------------------------------------
 
+let amountBill = 0;
 let percent = 0;
 let peopleAmount = 0;
 let amount = 0;
 let allAmount = 0;
+
 // ----------------------------------------------------
 // ----------------------------------------------------
 // ----------------------------------------------------
 // ----------------------------------------------------
 
 
-let amountBill = 0;
+
 function checkValue() {
   console.log(amountBill)
   
 }
+const clicked = document.getElementsByClassName('click');
 
 function tipCalculation () {
-  console.log(amountBill /= 2)
+  // console.log(amountBill = (amountBill * clicked[0].value) / 100);
+  if (input.value == '') {
+    console.log('input is empty')
+  } else if (amountBill > 0 && document.querySelector('button').classList.contains('click')) {
+    console.log(amountBill = amountBill * document.querySelector("#bill").value)  
+  }
 }
+
+
+// get value of input 
+input.oninput = function () {
+  amountBill = input.value;
+  checkValue();
+  tipCalculation();
+  
+};
 
 // click remove-add on percent buttons
 function removeClick() {
@@ -32,24 +49,26 @@ function removeClick() {
   }
 }
 
-
-
-
-input.oninput = function () {
-  amountBill = input.value;
-  checkValue();
-  tipCalculation();
-};
-
-
-
+// clicking on tip buttons
 for (let i = 0; i < tipButton.length; i++) {
   tipButton[i].addEventListener("click", (h) => {
     removeClick();
+    // class add when we're clicking
     h.target.classList.add("click");
-    console.log(h.target.value)
+    let targetValue = h.target.value
+    console.log(targetValue);
+
+
+    // if button contains click class
+    const active = document.querySelector('button')
+    const isActive = active.classList.contains("click");
+    // 
+    if(input.value == '') {
+      console.log('its empty')      
+    } else if (isActive == 'true') {
+      tipCalculation();
+    }
+
   });
 }
-
-
 // ----------------------------------------------------
