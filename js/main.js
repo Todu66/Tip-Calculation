b97d5ec5b462b91f6667456b3a723ab44d9d8041
Todu66 <giga.todua06@gmail.com>
2023-02-15 22:31:42 +0400

    adding calculation

--- js/main.js
+++ js/main.js
@@ -1,74 +1,86 @@
-const tipButton = document.querySelectorAll(".same");
-const resetButton = document.querySelector("#reset");
-const input = document.querySelector("#bill");
-const persentCal = document.getElementsByClassName("same");
-// ----------------------------------------------------
-// ----------------------------------------------------
+const buttons = document.querySelectorAll(".same");
 
-let amountBill = 0;
-let percent = 0;
-let peopleAmount = 0;
-let amount = 0;
-let allAmount = 0;
+let billInput = document.getElementById("bill");
+let billValue = 0;
+let numberInput = document.getElementById("numpeople");
+let numberValue = 0;
 
-// ----------------------------------------------------
-// ----------------------------------------------------
-// ----------------------------------------------------
-// ----------------------------------------------------
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // get the tip percentage from the button value
+    const tipPercentage = button.value; // = buttons with %
 
+    // get the bill amount from the bill input field
+    const bill = document.getElementById("bill").value; // = first input Bill
 
+    // get the number of people from the numpeople input field
+    const numPeople = document.getElementById("numpeople").value; // = second input with num of people
 
-function checkValue() {
-  console.log(amountBill)
-  
-}
-const clicked = document.getElementsByClassName('click');
+    // calculate the tip amount and total amount
 
-function tipCalculation () {
-  // console.log(amountBill = (amountBill * clicked[0].value) / 100);
-  if (input.value == '') {
-    console.log('input is empty')
-  } else if (amountBill > 0 && document.querySelector('button').classList.contains('click')) {
-    console.log(amountBill = amountBill * document.querySelector("#bill").value)  
-  }
-}
-
-
-// get value of input 
-input.oninput = function () {
-  amountBill = input.value;
-  checkValue();
-  tipCalculation();
-  
-};
-
-// click remove-add on percent buttons
-function removeClick() {
-  for (let i = 0; i < tipButton.length; i++) {
-    tipButton[i].classList.remove("click");
-  }
-}
-
-// clicking on tip buttons
-for (let i = 0; i < tipButton.length; i++) {
-  tipButton[i].addEventListener("click", (h) => {
-    removeClick();
-    // class add when we're clicking
-    h.target.classList.add("click");
-    let targetValue = h.target.value
-    console.log(targetValue);
-
-
-    // if button contains click class
-    const active = document.querySelector('button')
-    const isActive = active.classList.contains("click");
-    // 
-    if(input.value == '') {
-      console.log('its empty')      
-    } else if (isActive == 'true') {
-      tipCalculation();
-    }
-
+    const totalPerson = bill / numPeople;
+    const tipAmount = (totalPerson * tipPercentage) / 100;
+    // display the tip amount and total amount on the page
+    document.getElementById("tip-amount").innerHTML = tipAmount.toFixed(2);
+    document.getElementById("total-amount").innerHTML = totalPerson.toFixed(2);
   });
-}
-// ----------------------------------------------------
+});
+
+// add event listener to reset button
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", () => {
+  // reset all input fields
+  document.getElementById("bill").value = "";
+  document.getElementById("numpeople").value = "";
+
+  // clear tip amount and total amount
+  document.getElementById("tip-amount").textContent = "$0.00";
+  document.getElementById("total-amount").textContent = "$0.00";
+
+  document.getElementById('alert-zero').style.opacity = '0'
+});
+
+// make shure user doesnt enter negative numbers
+numberInput.addEventListener("input", function () {
+  numberValue = Math.max(0, this.value);
+  this.value = numberValue;
+});
+
+billInput.addEventListener("input", function () {
+  billValue = Math.max(0, this.value);
+  this.value = billValue;
+});
+
+// warning appereance
+
+billInput.addEventListener("click", function () {
+  if (this.value == 0) {
+    let message = document.getElementById("alert-zero");
+    
+    message.style.opacity = '1';
+  
+
+
+    // add change event listener to remove message when input value changes
+    this.addEventListener("change", function () {
+      if (this.value != 0) {
+        message.style.opacity = '0'
+      }
+    });
+  }
+});
+
+numberInput.addEventListener("click", function () {
+    if (this.value == 0) {
+      let message = document.getElementById("alert-zero2");
+  
+      message.style.opacity = '1'
+      // add change event listener to remove message when input value changes
+      this.addEventListener("change", function () {
+        if (this.value != 0) {
+          message.style.opacity = 0;
+        }
+      });
+    }
+  });
+  
